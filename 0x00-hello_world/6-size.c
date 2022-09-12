415fd2b4954a20efd93890fec74800572e86e49d
abdelrahman <abdelrahmannasser@gmail.com>
2022-09-12 00:35:51 -0700

    sizing

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,10 +14,10 @@ int main(void)
 	long li;
 	long long lli;
 	float f;
-printf("Size of a char: %ld byte(s)\n", sizeof(c));
-printf("Size of an int: %ld byte(s)\n", sizeof(i));
-printf("Size of a long int: %ld byte(s)\n", sizeof(li));
-printf("Size of a long long int: %ld byte(s)\n", size of(lli));
-printf("Size of a float: %ld byte(s)\n", sizeof(f));
+printf("Size of a char: %d byte(s)\n", sizeof(c));
+printf("Size of an int: %d byte(s)\n", sizeof(i));
+printf("Size of a long int: %d byte(s)\n", sizeof(li));
+printf("Size of a long long int: %d byte(s)\n", size of(lli));
+printf("Size of a float: %d byte(s)\n", sizeof(f));
 return (0);
 }
