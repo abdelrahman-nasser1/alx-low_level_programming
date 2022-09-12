a49ed6511b6f2de0b7f87ecb87dd063aca34b6d9
abdelrahman <abdelrahmannasser@gmail.com>
2022-09-12 09:55:44 -0700

    alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,13 +8,13 @@ int main(void)
 {
 char abc;
 
-for (abc = 'a'; abc<= 'z'; abc++)
+for (abc = 'a'; abc <= 'z'; abc++)
 {
 putchar(abc);
 }
 for (abc = 'A'; abc <= 'Z'; abc++)
 {
-putchar(abc;
+putchar(abc);
 }
 putchar('\n');
 return (0);
