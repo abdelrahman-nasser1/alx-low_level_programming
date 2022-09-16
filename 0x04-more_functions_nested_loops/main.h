a3a01ad834383c1649cf5cd3fa39a66ff0f48916
abdelrahman <abdelrahmannasser@gmail.com>
2022-09-16 13:08:38 -0700

    supper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File - main.h
+ * Author: abdelrahman nasser
+ * Desc: Header contains protypes for all functions
+ *      used in 0x03=more_functions_nested_loos directory
+ */
+
+int _putchar(char c);
+
+
+
+
+
+
+
+#endif
