528cebdc5c20a8a5aeae08fee71afd3d38b0cd5c
abdelrahman <abdelrahmannasser@gmail.com>
2022-09-19 16:25:45 -0700

    strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -9,12 +9,12 @@
 
 int _strlen(char *s)
 {
-	int len = 0;
-        
-	while (s[len] != '\0')
-        {
-	        len++;
-        }
+	 int len = 0;
+	 
+	 while (s[len] != '\0')
+	 {
+	         len++;
+	 }
 
 	return (len);
 }
