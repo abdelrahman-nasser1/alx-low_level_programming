2c65db148fc7fd3d0d24983674520e316cf4f8a6
abdo <abdelrahman-nasser1>
2024-01-24 17:06:39 -0800

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -3,8 +3,8 @@
 /**
  * int_index - return index place if comparison = true, else -1
  * @array: array
- *size: size of elements in array
- *@cmp: pointer to func of one of the 3 in main
+ * @size: size of elements in array
+ * @cmp: pointer to func of one of the 3 in main
  * Return: 0
  */
 int int_index(int *array, int size, int (*cmp)(int))
