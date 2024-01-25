62370989f7ebf7b874be481fd837fc7dd949fd06
abdo <abdelrahman-nasser1>
2024-01-24 16:12:44 -0800

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,57 +1,15 @@
 #include <stdio.h>
 #include "function_pointers.h"
-
 /**
- * print_name_as_is - prints a name as is
- * @name: name of the person
- *
- * Return: Nothing.
+ * print_name - print name using pointer to function
+ * @name: string to add
+ *@f: pointer to function
+ * Return: nothing
  */
-void print_name_as_is(char *name)
+void print_name(char *name, void(*f)(char *))
 {
-    printf("Hello, my name is %s\n", name);
-}
+	if (name == NULL || f == NULL)
+		return;
 
-/**
- * print_name_uppercase - print a name in uppercase
- * @name: name of the person
- *
- * Return: Nothing.
- */
-void print_name_uppercase(char *name)
-{
-    unsigned int i;
-
-    printf("Hello, my uppercase name is ");
-    i = 0;
-    while (name[i])
-    {
-        if (name[i] >= 'a' && name[i] <= 'z')
-        {
-            putchar(name[i] + 'A' - 'a');
-        }
-        else
-        {
-            putchar(name[i]);
-        }
-        i++;
-    }
+	f(name);
 }
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_name("Bob", print_name_as_is);
-    print_name("Bob Dylan", print_name_uppercase);
-    printf("\n");
-    return (0);
-}
-julien@ubuntu:~/0x0e. Function pointers$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-print_name.c -o a
-julien@ubuntu:~/0x0e. Function pointers$ ./a 
-Hello, my name is Bob
-Hello, my uppercase name is BOB DYLAN
-julien@ubuntu:~/0x0e. Function pointers$
