2912acbf37a49f54d6524b0d3bbe85f75eb17fdc
abdo <abdelrahman-nasser1>
2024-01-24 17:43:32 -0800

    3-main.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+/**
+ * main - Prints the result of simple operations.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
+ */
+int main(int attribute((unused)) argc, char *argv[])
+        {
+                int num1, num2;
+                char *op;
+                if (argc != 4)
+                {
+                        printf("Error\n");
+                        exit(98);
+                }
